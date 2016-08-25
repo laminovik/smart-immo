ca996b98ccf2c13f42da5cd3b6b0d717b2b8b5e5
amine <amine.benyessef@gmail.com>
2016-08-25 15:47:29 +0200

    modif de districts views et controller pour remplacer methods par requête BdD

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -8,9 +8,9 @@ class DistrictsController < ApplicationController
   	@district=District.find_by_id(params[:district])
   	@surface=params[:surface]
   	@type=params[:type]
-  	@sqm_price_buy = District.find_by_id(params[:district]).buy_sqm_price 
+  	@sqm_price_buy = District.find_by_id(params[:district]).sqm_buy 
   	@price= @sqm_price_buy*@surface.to_i
-    @sqm_price_rent = District.find_by_id(params[:district]).rent_sqm_price 
+    @sqm_price_rent = District.find_by_id(params[:district]).sqm_rent
     @rent= @sqm_price_rent*@surface.to_i
 
   end
