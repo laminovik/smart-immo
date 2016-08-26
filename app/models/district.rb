ef3540a825e2f24b95e74a598887c4d0fdde072a
amine <amine.benyessef@gmail.com>
2016-08-26 19:51:09 +0200

    détails dans les évaluations de bien

--- app/models/district.rb
+++ app/models/district.rb
@@ -24,4 +24,14 @@ class District < ActiveRecord::Base
     end
     return valid
   end
+
+  # def sales_low_end
+  #   total=sales.count
+  #   sales.order(:sqm_price).limit(total/5).average(:sqm_price).to_i
+  # end
+
+  #   def sales_high_end
+  #   total=sales.count
+  #   sales.order(sqm_price: :desc).limit(total/5).average(:sqm_price).to_i
+  # end
 end
