9c05390423f0c159c1391c68705319f6ac23b532
amine <amine.benyessef@gmail.com>
2016-09-17 12:45:32 +0200

    responsive views

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,11 +1,11 @@
 class CitiesController < ApplicationController
   def index
-  	@cities=City.includes(:sales, :rentals)
+  	@cities=City.includes(:sales, :rentals).sort_by(&:name)
   end
 
   def show
   	@city=City.find(params[:id])
-  	@districts=@city.valid_districts(40)
+  	@districts=@city.valid_districts(40).sort_by(&:name)
   end
   
 end
